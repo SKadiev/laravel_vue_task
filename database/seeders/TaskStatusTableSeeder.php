bf177e214d69509eff68ca21be43492c442ef78a
SKadiev <kadievstojan@gmail.com>
2022-07-19 21:54:01 +0200

    Step 3 Seeders setup and task model

---
+++ database/seeders/TaskStatusTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TaskStatusTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('task_status')->insert(
+            [
+                'status_name' => 'open',
+            ],
+            [
+                'status_name' => 'completed',
+            ],
+            [
+                'status_name' => 'cancelled',
+            ]
+        );
+    }
+}
